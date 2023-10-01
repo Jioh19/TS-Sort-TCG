62ce98924b9e94bae8883ec2440ded54e8e29d77
Jioh19 <jioh1982@gmail.com>
2023-10-01 17:30:57 -0300

    refactor

---
+++ build/NumbersCollection.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NumbersCollection = void 0;
+class NumbersCollection {
+    constructor(data) {
+        this.data = data;
+    }
+    get length() {
+        return this.data.length;
+    }
+    compare(leftIndex, rightIndex) {
+        return this.data[leftIndex] > this.data[rightIndex];
+    }
+    swap(leftIndex, rightIndex) {
+        [this.data[leftIndex], this.data[rightIndex]] = [
+            this.data[rightIndex],
+            this.data[leftIndex],
+        ];
+    }
+}
+exports.NumbersCollection = NumbersCollection;
