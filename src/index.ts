5f45dd9eb883801f06013997eabaf268ec720c09
Jioh19 <jioh1982@gmail.com>
2023-10-07 19:09:28 -0300

    add linkedList sort

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 523, 999, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data);
+// const numbersCollection = new NumbersCollection([10, 523, 999, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xyaaabb');
-const sorterString = new Sorter(charactersCollection);
-sorterString.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection('Xyaaabb');
+// const sorterString = new Sorter(charactersCollection);
+// sorterString.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(49);
+
+const sorter3 = new Sorter(linkedList);
+sorter3.sort();
+linkedList.print();
