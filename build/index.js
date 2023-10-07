1e259bc778de608091b1ba15d49c4bc6c51d2061
Jioh19 <jioh1982@gmail.com>
2023-10-07 17:51:04 -0300

    add string sort

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 523, 999, 3, -5, 0]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('Xyaaabb');
+const sorterString = new Sorter_1.Sorter(charactersCollection);
+sorterString.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 523, 999, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xyaaabb');
+const sorterString = new Sorter(charactersCollection);
+sorterString.sort();
+console.log(charactersCollection.data);
